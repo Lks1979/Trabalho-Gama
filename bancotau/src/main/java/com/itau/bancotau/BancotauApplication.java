2a4a3374e49c2ffd054f95d5b1e35aafc4d9984c
LukasOliveira <luka.oliver.1979@hotmail.com>
2021-09-21 11:01:15 -0300

    comit

---
+++ bancotau/src/main/java/com/itau/bancotau/BancotauApplication.java
@@ -0,0 +1,13 @@
+package com.itau.bancotau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancotauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancotauApplication.class, args);
+	}
+
+}
---
+++ bancotau/src/test/java/com/itau/bancotau/BancotauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.bancotau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancotauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
