bba73727af8bb5303ea4df0053ff7dab632f9902
LukasOliveira <luka.oliver.1979@hotmail.com>
2021-09-27 19:45:21 -0300

    commit

--- bancotau/src/main/java/com/itau/bancoitau/controler/ContaControler.java
+++ bancotau/src/main/java/com/itau/bancoitau/controler/ContaControler.java
@@ -3,6 +3,7 @@ package com.itau.bancoitau.controler;
 import java.util.List;
 
 import com.itau.bancoitau.model.Conta;
+import com.itau.bancoitau.repository.ContaRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController //Indica que a classe é um Controler Rest
 @RequestMapping("/conta") //Toda vez que chegar com a requisição desse tipo (/user) ele aciona public class ContaControler
